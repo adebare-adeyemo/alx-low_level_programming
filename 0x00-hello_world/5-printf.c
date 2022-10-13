59960fd52ff223bf381e3bac6a9a5a7a12f41922
jadvdm <oluwajadbb@gmail.com>
2022-10-13 02:33:50 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+/**
+ *
+ *this writes a very short word using printf
+ */
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  return(0);
+}

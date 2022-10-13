c93a2ff97ae3ef6b67ca555264f5027048b3c99f
jadvdm <oluwajadbb@gmail.com>
2022-10-13 02:19:30 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main is the begining point 
+ *
+ *Return value should be zero
+ *
+ *we use put instead of print
+ */
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
+}

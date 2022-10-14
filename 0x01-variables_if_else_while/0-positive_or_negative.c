2c5a75e8f2b048f733540a9826b056575d408b17
jadvdm <oluwajadbb@gmail.com>
2022-10-14 03:39:58 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,9 +14,9 @@ srand(time(0));
 n = rand() - RAND_MAX  / 2;
 if (n > 0)
 printf("%d is positive\n", n);
-else if (n < 0)
-printf("%d is negative\n", n);
 else if (n == 0)
+printf("%d is zero\n", n);
+else if (n < 0)
 printf("%d is negative\n", n);
 return (0);
 }

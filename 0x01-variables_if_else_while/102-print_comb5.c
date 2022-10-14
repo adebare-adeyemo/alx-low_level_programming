f6bd4e8bc30036cbefd91e500de08ddd5d572af1
Adebare Adeyemo <113603612+Jadvdm@users.noreply.github.com>
2022-10-14 19:15:35 +0100

    Create 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - entry point to vawulence program 
+ *
+ * Return: alway returns 0 
+ *
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 99; a++)
+	{
+		for (b = a + 1; b <= 99; b++)
+		{
+			putchar(a / 10 + '0');
+			putchar(a % 10 + '0');
+			putchar(' ');
+			putchar(b / 10 + '0');
+			putchar(b % 10 + '0');
+
+			if (!(a == 98 && b == 99))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

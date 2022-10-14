bbb1aba9092545e22a2e9c701981a418c7ee3cfc
jadvdm <oluwajadbb@gmail.com>
2022-10-14 07:07:35 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - is the beginng point of the program
+*Return: value is zero
+*/
+int main(void)
+{
+  /* start the vawulence */
+char a;
+for (a = 'a'; a <= 'z'; a++)
+putchar(a);
+putchar('\n');
+return (0);
+}

b0d118967c01139312a4d1cb516e224ad95d0cb2
jadvdm <oluwajadbb@gmail.com>
2022-10-14 07:20:54 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - starts the program
+*Return: value zero
+*/
+int main(void)
+/* comrade the program begins with full vawulence */
+{
+char a;
+for (a = 'a'; a <= 'z'; a++)
+putchar(a);
+for (a = 'A'; a <= 'Z'; a++)
+putchar(a);
+putchar('\n');
+return (0);
+}

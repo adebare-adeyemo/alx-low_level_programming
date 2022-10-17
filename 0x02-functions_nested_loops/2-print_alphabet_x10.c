1c7a1fc27875880bf1f85eab94852d6d4517a9b9
Adebare Adeyemo <113603612+Jadvdm@users.noreply.github.com>
2022-10-18 00:23:59 +0100

    Create 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*print_alphabet_x10 - acts as the entry point
+*Return: zero always
+*/
+Void print_alphabet_x10(void)
+{
+        char a;
+        int n = 0;
+
+        while (n < 9)
+        {
+               for (a = 'a'; a <= 'z'; a++;)
+               {
+                      _putchar(a);
+               }
+        _putchar('\n');
+        i++;
+
+        }
+}

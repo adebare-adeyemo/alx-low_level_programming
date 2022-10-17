0de3afefaf275fe7f2c5142cbae9d17f5a64a1bc
Adebare Adeyemo <113603612+Jadvdm@users.noreply.github.com>
2022-10-17 23:52:09 +0100

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include main.h
+
+/**
+*main - act as the beginning point of the putchar
+*Return: always zero
+*/
+int main(void)
+{
+        _putchar('_');
+        _putchar('p');
+        _putchar('u');
+        _putchar('t');
+        _putchar('c');
+        _putchar('h');
+        _putchar('a');
+        _putchar('r');
+
+	return (0);
+}

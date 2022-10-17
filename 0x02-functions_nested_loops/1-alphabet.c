c9b8fede68ae9739096d7a337e723b85ee21e3de
Adebare Adeyemo <113603612+Jadvdm@users.noreply.github.com>
2022-10-18 00:06:58 +0100

    Create 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include 'main.h'
+
+/**
+*main- act as the beginning point 
+*Return: always return 0
+*/
+void print_alphabet(void)
+{
+        char b = 'a';
+        while (b <= 'z')
+        {
+               _putchar(b);
+               b++;
+        }
+        _putchar('\n')
+
+}

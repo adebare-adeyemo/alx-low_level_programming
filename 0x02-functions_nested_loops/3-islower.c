99402b673b6998fdace64602bc7aab8c4142284b
Adebare Adeyemo <113603612+Jadvdm@users.noreply.github.com>
2022-10-18 02:16:11 +0100

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -7,9 +7,10 @@
 */
 int _islower(int c)
 {
-      if(c >= 'a' && c <= 'z')
-      {
-           return (1);
-      }
-      return (0);
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+
+	return (0);
 }

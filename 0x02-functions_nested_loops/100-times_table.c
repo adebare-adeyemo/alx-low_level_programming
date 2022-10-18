15a631e2ad9e3c14613bdbc05b099947a998cb62
Adebare Adeyemo <113603612+Jadvdm@users.noreply.github.com>
2022-10-18 12:27:42 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,38 +7,46 @@
   *
   * Return: Always 0.
   */
-void print_to_98(int n)
+void print_times_table(int n)
 {
-	if (n <= 98)
+	int x, y, z;
+
+	if (n >= 0 && n <= 14)
 	{
-		for (; n <= 98; n++)
+		for (x = 0; x <= n; x++)
 		{
-			if (n == 98)
+			for (y = 0; y <= n; y++)
 			{
-				printf("%d", n);
-				printf("\n");
-				break;
+				z = x * y;
+				if (z > 99)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar((z / 100) + '0');
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+				else if (z > 9)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar(32);
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+				else
+				{
+					if (y != 0)
+					{
+						_putchar(',');
+						_putchar(32);
+						_putchar(32);
+						_putchar(32);
+					}
+					_putchar(z + '0');
+				}
 			}
-			else
-			{
-				printf("%d, ", n);
-			}
-		}
-	}
-	else
-	{
-		for (; n >= 98; n--)
-		{
-			if (n == 98)
-			{
-				printf("%d", n);
-				printf("\n");
-				break;
-			}
-			else
-			{
-				printf("%d, ", n);
-			}
+			_putchar('\n');
 		}
 	}
 }

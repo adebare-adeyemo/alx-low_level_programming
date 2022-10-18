f6b35faced2a8b874db8d9c94eeb35e2ec13695d
Adebare Adeyemo <113603612+Jadvdm@users.noreply.github.com>
2022-10-18 12:57:27 +0100

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,31 +6,37 @@
  */
 void times_table(void)
 {
-	int i;
-	int j;
-	int table = j * i;
+	int x, y, z, u, d;
 
-	for (i = 0; i <= 9; i++)
+	for (x = 0; x <= 9; x++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (y = 0; y <= 9; y++)
 		{
-			if (j == 0)
+			z = x * y;
+
+			if (z > 9)
 			{
-				_putchar('0');
-			} else if (table <= 9)
+				u = z % 10;
+				d = (z - u) / 10;
+
+				_putchar(44);
+				_putchar(32);
+				_putchar(d + '0');
+				_putchar(u + '0');
+			}
+			else
 			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(table + '0');
-			} else
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(table / 10 + '0');
-				_putchar(table % 10 + '0');
+				if (y != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
+
+				_putchar(z + '0');
 			}
 		}
+
 		_putchar('\n');
 	}
 }

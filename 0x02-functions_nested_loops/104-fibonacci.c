258f3590c1985075ca2e16b2de04f12bd5e02f81
Adebare Adeyemo <113603612+Jadvdm@users.noreply.github.com>
2022-10-18 12:32:36 +0100

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -14,14 +14,14 @@ int main(void)
 
 	printf("%.0f, ", a);
 	printf("%.0f, ", b);
-	while (counter < 98)
+	while (counter <= 98)
 	{
 		counter++;
 		printf("%.0f", c);
 		a = b;
 		b = c;
 		c = a + b;
-		if (counter < 98)
+		if (counter <= 98)
 		{
 			printf(", ");
 		}

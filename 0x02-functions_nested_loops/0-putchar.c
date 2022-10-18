1004ad0596a78d7340b17779da08097b06b602c6
jadvdm <oluwajadbb@gmail.com>
2022-10-17 17:52:39 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,24 +6,13 @@
 */
 int main(void)
 {
-<<<<<<< HEAD
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-=======
-        _putchar('_');
-        _putchar('p');
-        _putchar('u');
-        _putchar('t');
-        _putchar('c');
-        _putchar('h');
-        _putchar('a');
-        _putchar('r');
->>>>>>> 35ddf013fa08a27c476815ed9a921f2a7559c6e4
-	return (0);
+       _putchar('_');
+       _putchar('p');
+       _putchar('u');
+       _putchar('t');
+       _putchar('c');
+       _putchar('h');
+       _putchar('a');
+       _putchar('r');
+       return (0);
 }

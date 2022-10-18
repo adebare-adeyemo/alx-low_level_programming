39a361178a0e32fb6bedd41a429d275f56a36313
Adebare Adeyemo <113603612+Jadvdm@users.noreply.github.com>
2022-10-18 12:43:28 +0100

    Create 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,30 +1,49 @@
 #include <stdio.h>
+#define MAXULI9 10000000000
+
 /**
- * main - main function
+ * main - prints the first 100 Fibonacci numbers, starting with 1 and 2,
+ * followed by a new line, without using long long, arrays or dynamic
+ * allocation
  *
- * Return: nothing
+ * Return: 0 on success
  */
 int main(void)
 {
-	int counter = 2;
+	int i;
+	unsigned long onebeforefirst = 0, onebeforesecond = 0;
+	unsigned long twobeforefirst = 0, twobeforesecond = 0;
+	unsigned long currentfirst = 0, currentsecond = 0;
 
-	float a = 1;
-	float b = a + 1;
-	float c = a + b;
+	onebeforesecond = 2;
+	twobeforesecond = 1;
+	currentsecond = onebeforesecond + twobeforesecond;
 
-	printf("%.0f, ", a);
-	printf("%.0f, ", b);
-	while (counter <= 98)
+	printf("%lu", twobeforesecond);
+	printf(", %lu", onebeforesecond);
+	printf(", %lu", currentsecond);
+
+	for (i = 3; i < 98; i++)
 	{
-		counter++;
-		printf("%.0f", c);
-		a = b;
-		b = c;
-		c = a + b;
-		if (counter <= 98)
+		twobeforefirst = onebeforefirst;
+		twobeforesecond = onebeforesecond;
+
+		onebeforefirst = currentfirst;
+		onebeforesecond = currentsecond;
+
+		currentfirst = twobeforefirst + onebeforefirst;
+		currentsecond = twobeforesecond + onebeforesecond;
+
+		if (currentsecond >= MAXULI9)
 		{
-			printf(", ");
+			currentfirst++;
+			currentsecond -= MAXULI9;
 		}
+
+		if (currentfirst != 0)
+			printf(", %lu%010lu", currentfirst, currentsecond);
+		else
+			printf(", %lu", currentsecond);
 	}
 	printf("\n");
 	return (0);

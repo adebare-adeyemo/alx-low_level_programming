69f10270c9b5c516b48be99e690599b403ff9b12
Adebare Adeyemo <113603612+Jadvdm@users.noreply.github.com>
2022-10-20 13:35:53 +0100

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,8 +1,7 @@
 #include "main.h"
-
 /**
   * _isdigit - Check if a character is a digit
-  * @x: The number to be checked
+  * @c: The number to be checked
   *
   * Return: 1 for a character that will be a digit or 0 for any else
   */

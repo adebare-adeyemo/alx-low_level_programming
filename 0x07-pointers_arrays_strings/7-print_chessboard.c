84ad30d91f888dc588ed04f47d2fd6c5593d9bad
Adebare Adeyemo <113603612+Jadvdm@users.noreply.github.com>
2022-10-31 19:42:56 +0100

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,18 +1,22 @@
-#include "main.h"
-
+#include "holberton.h"
 /**
- * print_chessboard - Prints a chessboard.
- * @a: The chessboard to be printed.
+ * *print_chessboard - description
+ * @a: board
+ * Return: void
  */
+
+
 void print_chessboard(char (*a)[8])
 {
-	int indx1, indx2;
+int i, j;
 
-	for (indx1 = 0; a[indx1][7]; indx1++)
+for (i = 0; i < 8; i++)
+{
+	for (j = 0; j < 8; j++)
 	{
-		for (indx2 = 0; indx2 < 8; indx2++)
-			_putchar(a[indx1][indx2]);
-
-		_putchar('\n');
+		_putchar(a[i][j]);
 	}
+	_putchar('\n');
+}
+
 }

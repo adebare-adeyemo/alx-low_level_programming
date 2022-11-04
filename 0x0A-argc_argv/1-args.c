68ac23873755f6027caf0c13da4577a829862397
Adebare Adeyemo <113603612+Jadvdm@users.noreply.github.com>
2022-11-04 13:01:22 +0100

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,17 +1,15 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
-  * main - Prints its name
-  * @argc: argument count
-  * @argv: argument vector
-  *
-  * Return: Always zero
-  */
-int main(int argc, char *argv[])
+ * main - print the number of arguments passed to the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	(void) argc;
-	printf("%s\n", argv[0]);
-
+	printf("%d\n", argc - 1);
 	return (0);
 }
